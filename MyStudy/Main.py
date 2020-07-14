729be550701ebfa83a4dd631884bb9dbf25045d3
xDarharx <sakhramov@gmail.com>
2020-07-14 22:06:44 +0300

    new content

--- MyStudy/Main.py
+++ MyStudy/Main.py
@@ -1,4 +1,13 @@
-#!/usr/bin/python3.8
-import sys
-version = "Python %d.%d" % (sys.version_info.major, sys.version_info.minor)
-print("Hello World",version)
+name = input("Enter your name:")
+surname = input("Enter your surname:")
+age = int(input("Enter your age:"))
+weight = int(input("Enter your weight:"))
+
+if ((age < 30) and (50 < weight < 120)):
+    print(f"You are in good condition.")
+elif ((30 < age < 40) and (50 > weight > 120)):
+    print(f"You need some training.")
+elif ((age > 40) and (50 > weight > 120)):
+    print(f"You should visit your doctor.")
+else:
+    print(f"Do whatever you want.")
