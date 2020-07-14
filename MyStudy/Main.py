bbb640af63c816494cc0f11483411f36136fa00e
xDarharx <sakhramov@gmail.com>
2020-07-14 21:22:27 +0300

    new content

--- MyStudy/Main.py
+++ MyStudy/Main.py
@@ -1,1 +1,3 @@
-print ("Hello World!")
+#!/usr/bin/python3.6
+import sys version = "Python %d.%d" % (sys.version_info.major, sys.version_info.minor)
+print("Hello, Red Hat Developer World from",version)
