318a498c89ae94dc9e329b5e3cadf309c6ed8fed
xDarharx <sakhramov@gmail.com>
2020-07-14 22:38:54 +0300

    1

--- MyStudy/Main.py
+++ MyStudy/Main.py
@@ -6,11 +6,11 @@ surname = input("Enter your surname:")
 age = int(input("Enter your age:"))
 weight = int(input("Enter your weight:"))
 
-if ((age < 30) and (50 < weight < 120)):
+if ((age < 30) and ((50 < weight) or (weight < 120))):
     print(f"You are in good condition.")
-elif ((30 < age < 40) and (50 > weight > 120)):
+elif ((30 < age < 40) and ((50 > weight) or (weight > 120))):
     print(f"You need some training.")
-elif ((age > 40) and (50 > weight > 120)):
+elif ((age > 40) and ((50 > weight) or (weight > 120))):
     print(f"You should visit your doctor.")
 else:
     print(f"Do whatever you want.")
