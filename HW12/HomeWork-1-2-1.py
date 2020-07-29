bf7a02d69a577a22fddb6d1e3cfb1c484f8e99b9
xDarharx <sakhramov@gmail.com>
2020-07-29 22:58:36 +0300

    HW121

---
+++ HW12/HomeWork-1-2-1.py
@@ -0,0 +1,11 @@
+# 1: Даны два произвольные списка. Удалите из первого списка элементы присутствующие во втором списке.
+
+my_list_1 = [2, 5, 8, 2, 12, 12, 4]
+my_list_2 = [2, 7, 12, 3]
+temp_list = []
+
+for num_list1 in my_list_1:
+    if num_list1 not in my_list_2:
+        temp_list.append(num_list1)
+    
+print(temp_list)
