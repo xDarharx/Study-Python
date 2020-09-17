58308b7d39de56e51631919a16eb7927fa21b0aa
xDarharx <sakhramov@gmail.com>
2020-09-17 20:52:44 +0300

    + nets.csv

---
+++ MyStudy/ntwk2ip.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.8
+
+# with open ('nets.csv', 'r') as nets_file:
+#     for line in nets_file:
+#         tmp_line = line.replace('\n', '')
+#         print (tmp_line)
+
+
+f = open('nets.csv','w')
