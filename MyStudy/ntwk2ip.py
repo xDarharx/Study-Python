c2d107545d7a033add83d97a825e820d3c4785a5
xDarharx <sakhramov@gmail.com>
2020-09-22 01:56:46 +0300

    add ping script

--- MyStudy/ntwk2ip.py
+++ MyStudy/ntwk2ip.py
@@ -1,30 +1,12 @@
 #!/usr/bin/python3.8
 
-with open ('nets.csv', 'r') as nets_file:
+import ipaddress, os
+
+with open ('./nets.csv', 'r') as nets_file:
     for line in nets_file:
-        tmp_line = line.replace('\n', '')
-        print (tmp_line)
-        ntwk_size = 2 ** (32 - (int(line[line.find('/')+1:line.find(';')])))
-        print (ntwk_size)
-        ntwk = line[:line.find('/')]
-        print (ntwk)
-
-        ntwk_addr = ntwk.split('.')
-        for i in range(0, len(ntwk_addr)):
-            ntwk_addr[i] = int(ntwk_addr[i]) 
-        print (ntwk_addr, '\n')
-
-        ip_addr = ntwk_addr
-        if ntwk_addr[3] + (ntwk_size) <= 256:
-            for i in range(0, ntwk_size - 1):
-                ip_addr[3] = ip_addr[3] + 1
-                print (ip_addr)
-        else:
-            for i in range(0, ntwk_size - 1):
-                ip_addr = ip_addr[3] + 1
-                print (ip_addr)
-
-
-
-
-        print ('==============================================\n')
+        with open ('./ip-addresses.csv', 'a') as ip_addr_file:
+            for ip_addr in ipaddress.IPv4Network(line.replace('\n', '')).hosts():
+                ip_addr = str(ip_addr)
+                #print (str(os.system('ping %s -n 1' % (ip_addr,))))
+                result = str(ip_addr),';',str((os.system('ping %s -n 1' % (ip_addr,)))),';\n'
+                ip_addr_file.writelines (result)
